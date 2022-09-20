13433b5f07a2b00cff438aab061f6635f8f53ead
tgmiau <113451244+tgmiau@users.noreply.github.com>
2022-09-20 15:15:32 -0300

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,7 @@
+#DILLIA ACEITUNO 4D
+a = int(input("INGRESE UN NUMERO: "))
+c = int(input("¿QUE MULTIPLOS QUIERES?"))
+for x in range(1,c+1):
+  resultado = x*a
+  print(("%d x %d =%d")%(a,x,resultado))
+
